b1c8550b3b43f9ef9c60e8cbf318aa0516205260
Phan Thành Chung <phanthanhchung8c8@gmail.com>
2026-07-04 18:47:24 +0700

    Remove base path for Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-	base: "/Frontend-Mentor-Browser-Extensions/",
 	plugins: [react()],
 	resolve: {
 		tsconfigPaths: true,
